c3c0d48e1428e86ce80188e921f150456e29a1e6
akoelbl <anton.koelbl@online.de>
2023-10-29 16:22:13 +0100

    Added Xcode project

---
+++ SliderSolorPicker/SliderSolorPicker/SliderSolorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderSolorPickerApp.swift
+//  SliderSolorPicker
+//
+//  Created by Anton Kölbl on 29.10.23.
+//
+
+import SwiftUI
+
+@main
+struct SliderSolorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
